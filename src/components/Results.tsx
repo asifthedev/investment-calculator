bb1f79f01ecc5efeeb0b77f4a235b3eadd2f45e0
Asif Shahzad <asifthedev@gmail.com>
2026-04-10 16:56:16 +0500

    Add new calculation in Result.jsx

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,5 @@
-import type { UserInputData, InvestmentResults } from "./types";
-import { calculateInvestmentResults, formatter } from "../utils/investment";
+import type { UserInputData, InvestmentResults } from './types';
+import { calculateInvestmentResults, formatter } from '../utils/investment';
 
 function Results({ userInput }: { userInput: UserInputData }) {
   const results: InvestmentResults[] = calculateInvestmentResults(userInput);
@@ -17,29 +17,28 @@ function Results({ userInput }: { userInput: UserInputData }) {
       </thead>
       <tbody>
         {results.map((eachYearData) => {
+          //  initialInvestment = valueEndOfYear - annualInvestment - interest
           const initialInvestment =
-            results[0].valueEndOfYear -
-            results[0].annualInvestment -
-            results[0].interest;
+            results[0].valueEndOfYear - results[0].annualInvestment - results[0].interest;
+
+          // Formula: totalInterest = valueEndOfYear - (initialInvestment + annualInvestment × year)
+
+          const totalInterest =
+            eachYearData.valueEndOfYear -
+            (initialInvestment + eachYearData.annualInvestment * eachYearData.year);
+
+          //  totalInvestedCapital = initialInvestment + (annualInvestment × year)
+
+          const totalInvestedCapital =
+            initialInvestment + eachYearData.annualInvestment * eachYearData.year;
 
           return (
             <tr>
               <td>{eachYearData.year}</td>
               <td>{formatter.format(eachYearData.valueEndOfYear)}</td>
               <td>{formatter.format(eachYearData.interest)}</td>
-              <td>
-                {formatter.format(
-                  eachYearData.valueEndOfYear -
-                    (eachYearData.year * eachYearData.annualInvestment +
-                      initialInvestment),
-                )}
-              </td>
-              <td>
-                {formatter.format(
-                  initialInvestment +
-                    eachYearData.year * eachYearData.annualInvestment,
-                )}
-              </td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalInvestedCapital)}</td>
             </tr>
           );
         })}
